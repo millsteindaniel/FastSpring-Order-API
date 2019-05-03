10a36d33ed872a5297a11b5a3cecc4f42ae20355
Danny Millstein <dmillstein@umail.ucsb.edu>
2019-05-03 16:05:19 -0700

    First commit

---
+++ orderProcessingService/src/main/java/processing/order/orderProcessingService/OrderProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package processing.order.orderProcessingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingServiceApplication.class, args);
+	}
+
+}
